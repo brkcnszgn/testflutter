bb4fb6451efef5d0ba16948fa32f2d892b11ed91
Burakcan Sezgin <brkcnszgn@gmail.com>
2019-10-20 18:41:21 +0300

    fluuter Statefull calisma mantigi

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,34 @@ import 'home.dart';
 void main() {
   //Custom Widget disaridan statefull olarak olusturulup main methodu icersinde tanimlanarak uygulama icin return edilip calistirabilir.
   var customWidget = CustomWidget();
-  runApp(
-    new MaterialApp(
+  var count = 0;
+  runApp(myHomePage()); //runApp methodu gelen widget methodunu ekranda gosterir
+}
+
+// ignore: camel_case_types
+class myHomePage extends StatefulWidget {
+  int count = 0;
+
+  void Count() {
+    debugPrint("Count Degistirildi");
+    count++;
+  }
+
+  //StateFull alaninin cache alani denebilir.
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("Create State tetiklendi");
+    // TODO: implement createState
+    return MyHomePage();
+  }
+}
+
+class MyHomePage extends State<myHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("build methodu tetiklendi");
+    // TODO: implement build
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.amber, accentColor: Colors.amber),
       home: Scaffold(
@@ -21,66 +47,59 @@ void main() {
             style: TextStyle(fontSize: 18.0),
           ),
         ),
-        floatingActionButton: _floatingButton,
-        body: customWidget,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("Buttona tiklandi");
+            setState(() {
+              debugPrint("FloatButton setState calisti");
+              widget.Count();
+            });
+          },
+          child: Icon(
+            Icons.add_circle_outline,
+            color: Colors.white,
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Click time for Button : "),
+              Text(
+                "${widget.count}",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  ); //runApp methodu gelen widget methodunu ekranda gosterir
+    );
+  }
 }
 
 Container _bodyContainer() {
   return Container(
-          color: Colors.redAccent,
-          constraints: BoxConstraints(
-            minWidth: 250,
-            minHeight: 250,
-            maxWidth: 250,
-            maxHeight: 350,
-          ),
-          child: Card(
-            margin: EdgeInsets.all(20.0),
-            elevation: 12.0,
-            child: Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+      color: Colors.redAccent,
+      constraints: BoxConstraints(
+        minWidth: 250,
+        minHeight: 250,
+        maxWidth: 250,
+        maxHeight: 350,
+      ),
+      child: Card(
+        margin: EdgeInsets.all(20.0),
+        elevation: 12.0,
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Row(
                   mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          child: Container(
-                            child: Icon(
-                              Icons.add_circle_outline,
-                              size: 35.0,
-                              color: Colors.white,
-                            ),
-                            color: Colors.pinkAccent,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Icon(
-                              Icons.add_circle_outline,
-                              size: 35.0,
-                              color: Colors.black,
-                            ),
-                            color: Colors.pinkAccent,
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: Icon(
-                              Icons.add_circle_outline,
-                              size: 35.0,
-                              color: Colors.yellowAccent,
-                            ),
-                            color: Colors.pinkAccent,
-                          ),
-                        ),
-                      ],
-                    ),
                     Expanded(
                       child: Container(
                         child: Icon(
@@ -89,9 +108,7 @@ Container _bodyContainer() {
                           color: Colors.white,
                         ),
                         color: Colors.pinkAccent,
-
                       ),
-                      flex: 5,
                     ),
                     Expanded(
                       child: Container(
@@ -102,7 +119,6 @@ Container _bodyContainer() {
                         ),
                         color: Colors.pinkAccent,
                       ),
-                      flex: 2,
                     ),
                     Expanded(
                       child: Container(
@@ -113,28 +129,46 @@ Container _bodyContainer() {
                         ),
                         color: Colors.pinkAccent,
                       ),
-                      flex: 8,
                     ),
-                  ]),
-            ),
-          ));
+                  ],
+                ),
+                Expanded(
+                  child: Container(
+                    child: Icon(
+                      Icons.add_circle_outline,
+                      size: 35.0,
+                      color: Colors.white,
+                    ),
+                    color: Colors.pinkAccent,
+                  ),
+                  flex: 5,
+                ),
+                Expanded(
+                  child: Container(
+                    child: Icon(
+                      Icons.add_circle_outline,
+                      size: 35.0,
+                      color: Colors.black,
+                    ),
+                    color: Colors.pinkAccent,
+                  ),
+                  flex: 2,
+                ),
+                Expanded(
+                  child: Container(
+                    child: Icon(
+                      Icons.add_circle_outline,
+                      size: 35.0,
+                      color: Colors.yellowAccent,
+                    ),
+                    color: Colors.pinkAccent,
+                  ),
+                  flex: 8,
+                ),
+              ]),
+        ),
+      ));
 }
-
-/*Notlar
-
- Yararli kisayollar Alt + enter
-
- --Ekrana sigmayan resimler icin IntrinsicHeight(child : kullanmak zorundayiz)
-  Internetten ya da servisten gec yuklenen resimler icin FadeInImage.assetNetwork(yuklenmeden onceki gosterilecek resim, gosterilecek resim) seklinde karmasa onlenebilir.
-
- Kullanilan widgetlar icinde istege gore yerlestirmek isteniyorsa en disini Expanded widgeti ile sarilmali
- Bottom degeri 1 top degeri -1 right degeri 1 left degeri 0
-Belirlenin widget her nerede gosterilecekse 0.1 0.2 gibi seklinde tasinabilir.
-bunlar ornek olarak kisayollari ise Bottom.center start.center gibi olabilir.
- disarda widget olusturup iceriye fonksiyonunu yazabiliriz bu sayede
-karmasa olmayacaktir
-
- */
 
 Widget get _floatingButton {
   return FloatingActionButton(
@@ -156,3 +190,29 @@ Widget get _centerText {
     ),
   );
 }
+/*Notlar
+
+ Yararli kisayollar Alt + enter
+
+ --Ekrana sigmayan resimler icin IntrinsicHeight(child : kullanmak zorundayiz)
+  Internetten ya da servisten gec yuklenen resimler icin FadeInImage.assetNetwork(yuklenmeden onceki gosterilecek resim, gosterilecek resim) seklinde karmasa onlenebilir.
+
+ Kullanilan widgetlar icinde istege gore yerlestirmek isteniyorsa en disini Expanded widgeti ile sarilmali
+ Bottom degeri 1 top degeri -1 right degeri 1 left degeri 0
+Belirlenin widget her nerede gosterilecekse 0.1 0.2 gibi seklinde tasinabilir.
+bunlar ornek olarak kisayollari ise Bottom.center start.center gibi olabilir.
+ disarda widget olusturup iceriye fonksiyonunu yazabiliriz bu sayede
+karmasa olmayacaktir
+
+//stateless ve ya statefull widgetlar icerisinde parametreleri zorunlu kilabiliriz. Bunun icin
+//olusturulan custom widget icinde
+ ornegin
+ String title;
+  myHomePage({String title}){
+  this.title = title;
+  }   bu sekilde constructor olusturulmus oldu.
+  ya da stateless icerisinde ise bu durum
+  final String title;
+  myHomePage({this.title}); seklinde tanimlanir.
+
+ */
---
+++ lib/statefull_widgets.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class customMyApp extends StatefulWidget {
+  @override
+  _customMyAppState createState() => _customMyAppState();
+}
+
+class _customMyAppState extends State<customMyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
